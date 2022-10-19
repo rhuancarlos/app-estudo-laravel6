63cbe212e4857d4f64759c78868c571047d0c40e
Rhuan Carlos <rhuancarlosg12@gmail.com>
2022-10-18 21:44:12 -0300

    Routes per Controller, Controller resources

--- routes/web.php
+++ routes/web.php
@@ -15,110 +15,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Padrão aceitável somente em casos de views estáticas.
-// Route::view('/view', 'home.welcome', ["id" => 2]);
-
-Route::post('/register', function () {
-    return 'register';
-});
-
-Route::get('contato', function () {
-    return 'contato';
-});
-
-Route::any('any', function () {
-    return 'any';
-});
-
-Route::match(['post', 'get'], 'match', function () {
-    return 'match';
-});
-
-Route::get('/login', function () {
-    return "Login!";
-})->name('login');
-
-//******************/ ROUTES WHEN PARAMETERS /*******************/
-Route::get('/categorias/{cat}', function ($parm1) {
-    return "Listando registros da categoria : {$parm1}";
-});
-
-Route::get('/categoria/{slug}/posts', function ($slug) {
-    return "Registros da categoria: {$slug}";
-});
-
-Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
-    return "Produto(s): {$idProduct}";
-});
-
-Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
-    return "Produto(s): {$idProduct}";
-});
-//***************************************************************/
-
-
-//******************/ ROUTES WITH REDIRECT /*******************/
-Route::redirect('acessar1','acessar2');
-
-// Route::get('acessar1', function () {
-//     return redirect("acessar1");
-// });
-
-Route::get('acessar2', function () {
-    return redirect()->route("url.name");
-});
-//***************************************************************/
-
-
-//****************/ ROUTES WITH PARAMETER "NAME" **********/
-Route::get('/name-of-url', function () {
-    return "Hey Guyz, Hello World !";
-})->name('url.name');
+//****************/ ROUTES USING CONTROLLERS ************/
+/*
+Route::delete('/products/{id}', 'ProductController@destroy')->name('products.destroy');
+Route::put('/products/{id}', 'ProductController@update')->name('products.update');
+Route::get('/products/{id}/edit', 'ProductController@edit')->name('products.edit');
+Route::get('/products/create', 'ProductController@create')->name('products.create');
+Route::get('/products/{id}', 'ProductController@show')->name('products.show');
+Route::get('/products', 'ProductController@index')->name('products.index');
+Route::post('/products', 'ProductController@store')->name('products.store');
+*/
 //********************************************************/
 
-
-//******************/ ROUTES GROUPS /*********************/
-
-// Route::middleware(['auth'])->group(function () {
-//     /**
-//      * Neste caso quando definido um middleware diretamente ou em array,
-//      * o laravel irá aplica-lo antes de checar as rotas.
-//      */
-
-//     Route::prefix('admin')->group(function () { //Agrupamento por prefixo no nome das rotas
-
-//         Route::namespace('Admin')->group(function () { //Agrupamento por namespace p/ os controller
-
-//             Route::name('name.')->group(function () {
-                
-//                 Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
-        
-//                 Route::get('/finantical', 'TesteController@teste')->name('finantical');
-        
-//                 Route::get('/produtos', 'TesteController@products')->name('products');
-        
-//                 Route::get('/', function () {
-//                     return redirect()->route('admin.dashboard');
-//                 })->name('home');
-//             });
-//         });
-//     });
-// });
-
-Route::group([
-    'middleware' => [],
-    'prefix' => 'admin',
-    'namespace' => 'Admin',
-    'name' => 'admin.'
-], function () {
-    Route::get('/dashboard', 'TesteController@dashboard')->name('dashboard');
-
-    Route::get('/finantical', 'TesteController@finantical')->name('finantical');
-
-    Route::get('/products', 'TesteController@products')->name('products');
-
-    Route::get('/', function () {
-        return redirect()->route('dashboard');
-    })->name('home');
-});
+//****************/ ROUTES USING RESOURCES ***************/
+Route::resource('products', 'ProductController');
 //********************************************************/
